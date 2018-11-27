4adb62e519b40c848f40618544f8e3bc37abbbc4
Sakwer <patryk-szczesny@o2.pl>
2018-11-27 13:09:49 +0100

    Adding simple one button widget to home screen

---
+++ app/src/main/java/learningproject/pl/playmusicinflash/PlayInFlashAppWidgetProvider.java
@@ -0,0 +1,23 @@
+package learningproject.pl.playmusicinflash;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+public class PlayInFlashAppWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.play_in_flash_widget);
+            views.setOnClickPendingIntent(R.id.play_in_flash_button, pendingIntent);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
