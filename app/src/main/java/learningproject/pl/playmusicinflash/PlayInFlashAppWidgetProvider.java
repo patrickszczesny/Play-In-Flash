ead25007651213494a97b25bb1381af1d47ed86d
Sakwer <patryk-szczesny@o2.pl>
2018-12-01 00:48:09 +0100

    Putting texts into string.xml Making simple button on the widget to play careless asap.

--- app/src/main/java/learningproject/pl/playmusicinflash/PlayInFlashAppWidgetProvider.java
+++ app/src/main/java/learningproject/pl/playmusicinflash/PlayInFlashAppWidgetProvider.java
@@ -5,22 +5,45 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.widget.RemoteViews;
 
-import static android.support.v4.app.ActivityCompat.startActivityForResult;
 
 public class PlayInFlashAppWidgetProvider extends AppWidgetProvider {
+    public static String ACTION_WIDGET_RECEIVER = "ActionReceiverWidget";
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
-            Intent intent = new Intent(context, MainActivity.class);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.play_in_flash_widget);
+
+            Intent intentForMainActivity = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intentForMainActivity, 0);
             views.setOnClickPendingIntent(R.id.play_in_flash_button, pendingIntent);
+
+            Intent intentToPlayCarelessWhisper = new Intent(context, PlayInFlashAppWidgetProvider.class);
+            intentToPlayCarelessWhisper.setAction(ACTION_WIDGET_RECEIVER);
+            PendingIntent pendingIntentForCarelessWhisper = PendingIntent.getBroadcast(context, 0, intentToPlayCarelessWhisper, 0);
+            views.setOnClickPendingIntent(R.id.instant_play,pendingIntentForCarelessWhisper);
 
             appWidgetManager.updateAppWidget(appWidgetId, views);
 
         }
     }
-}
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        final String action = intent.getAction();
+        if (AppWidgetManager.ACTION_APPWIDGET_DELETED.equals(action)) {
+            final int appWidgetId = intent.getExtras().getInt(
+                    AppWidgetManager.EXTRA_APPWIDGET_ID,
+                    AppWidgetManager.INVALID_APPWIDGET_ID);
+            if (appWidgetId != AppWidgetManager.INVALID_APPWIDGET_ID) {
+                this.onDeleted(context, new int[] { appWidgetId });
+            }
+        } else {
+            if (intent.getAction().equals(ACTION_WIDGET_RECEIVER)) {
+                MediaPlayer carelessWhisperMediaPlayer = MediaPlayer.create(context,R.raw.wham_careless_whisper);
+                carelessWhisperMediaPlayer.start();
+            }
+            super.onReceive(context, intent);
+        }
+}}
